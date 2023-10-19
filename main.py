193e7da6dc50b743e229b4eca9e0837009fb476a
aniketdeogaonkar <aniketdeogaonkar@gmail.com>
2023-10-19 19:25:58 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.cnnKDClassification import logger
+from cnnKDClassification import logger
+from cnnKDClassification.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>stage {STAGE_NAME} started <<<<<<<")
+    obj =DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>stage {STAGE_NAME} completed <<<<<<<\n\nx====================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
